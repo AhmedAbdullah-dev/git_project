71968af914046c519268726e5181ad9af2260ac3
Ahmed <ahmedabdullah45379@gmail.com>
2026-04-24 16:29:27 +0200

    Edit on Tree sort

--- Tree_sort.cpp
+++ Tree_sort.cpp
@@ -2,67 +2,77 @@
 using namespace std;
 
 struct Node {
-    int key ;
+    int value;
     Node *left , *right;
 };
 
-struct Node *newNode (int item){
-    struct Node *temp = new Node ;
-    temp->key = item;
-    temp->left = temp->right = NULL;
+Node *newNode(int item){
+    Node *temp = new Node ;
+    temp->value = item ;
+    temp->left = temp->right = NULL ;
     return temp ;
-};
+}
 
-void storesort (Node *root , int arr[] , int &i){
+Node* insert(Node* node, int key) {
+    if (node == NULL) {
+        return newNode(key);
+    }
+
+    if (key < node->value) {
+        node->left = insert(node->left, key);
+    } 
+    else {
+        node->right = insert(node->right, key);
+    }
+
+    return node;
+}
+
+void storesort(Node *root , int arr[] , int &i){
     if (root != NULL){
-        storesort(root->left ,arr , i );
-        arr[i++] = root->key;
-        storesort(root->right , arr , i);
+    storesort(root->left , arr , i);
+
+    arr[i] = root->value;
+    i++;
+
+    storesort(root->right , arr , i);
     }
 }
 
-Node* insert(Node* node , int key){
-    if (node == NULL) return newNode (key);
-    if (key < node->key)
-        node->left = insert(node->left , key);
-    else 
-      if (key > node->key)
-      node->right = insert(node->right , key);
-      return node;
-}
-
-void treesort(int arr[] ,int n){
+void treesort(int arr[] , int n){
     Node *root = NULL;
     root = insert(root , arr[0]);
-    for (int i = 1; i < n; i++)
+
+    for (int i = 0; i < n; i++)
     {
         insert(root , arr[i]);
     }
+
     int i = 0;
     storesort(root , arr , i);
     
 }
 
-int main(){
-    int size;
-    cout << "\n Enter size : ";
-    cin >> size;
-    
-    int arr[size];
-    for (int i = 0; i < size; i++)
-    {
-        cout << "\n arr[" << i << "]= ";
-        cin >> arr[i];
-    }
+int main (){
+  int size;
+  cout << "Enter Size of Array : ";
+  cin >> size;
 
-    treesort(arr , size);
-    
-    for (int i = 0; i < size; i++)
-    {
-        cout << "\n" << arr[i];
-    }
+  int array[size];
+   for (int i = 0; i < size; i++)
+   {
+    cout << "Array [" << i << "]= ";
+    cin >> array[i];
+   }
 
-    return 0;
-    
-    
+   treesort(array , size);
+
+   cout << "Sortet Tree :";
+   for (int i = 0; i < size; i++)
+   {
+    cout << array[i] << " ";
+   }
+   
 }
+
+
--- git.c++
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-
-    cout << "my name is ahmed" << endl;
-
-    cout << "my age  20" << endl;
-}
